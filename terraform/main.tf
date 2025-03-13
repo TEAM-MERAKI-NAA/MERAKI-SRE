5c5dd8547d1ceaae8912c99f67f0de712a366ef6
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-03-13 18:27:11 -0400

    added few resources in  main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,15 @@
-# provider as Azure 
+# Provider Configuration
 provider "azurerm" {
   features {}
 }
 
-
-#Resource Group
+# Resource Group
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
 
-# Public Virtual Network
+# Virtual Networks
 resource "azurerm_virtual_network" "public_vnet" {
   name                = "${var.prefix}-public-vnet"
   address_space       = [var.public_vnet_cidr]
@@ -18,7 +17,6 @@ resource "azurerm_virtual_network" "public_vnet" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-# Private Virtual Network
 resource "azurerm_virtual_network" "private_vnet" {
   name                = "${var.prefix}-private-vnet"
   address_space       = [var.private_vnet_cidr]
@@ -26,8 +24,7 @@ resource "azurerm_virtual_network" "private_vnet" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-
-# Public Subnet
+# Subnets
 resource "azurerm_subnet" "public_subnet" {
   name                 = "${var.prefix}-public-subnet"
   resource_group_name  = azurerm_resource_group.main.name
@@ -35,11 +32,66 @@ resource "azurerm_subnet" "public_subnet" {
   address_prefixes     = [var.public_subnet_cidr]
 }
 
-
-# Private Subnet
 resource "azurerm_subnet" "private_subnet" {
   name                 = "${var.prefix}-private-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.private_vnet.name
   address_prefixes     = [var.private_subnet_cidr]
+}
+
+# App Service Plan
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "${var.prefix}-app-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Back-end App Service
+resource "azurerm_app_service" "backend_app" {
+  name                = "${var.prefix}-backend"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
+
+  site_config {
+    always_on = true
+  }
+
+  app_settings = {
+    "DATABASE_URL" = "YourDatabaseConnectionString"
+  }
+}
+
+# Storage Account for Front-end
+resource "azurerm_storage_account" "frontend_storage" {
+  name                     = "frontendstorage${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+# Application Insights for SRE
+resource "azurerm_application_insights" "app_insights" {
+  name                = "${var.prefix}-insights"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  application_type    = "web"
+}
+
+# PostgreSQL Database
+resource "azurerm_postgresql_server" "db_server" {
+  name                = "${var.prefix}-db"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "B_Gen5_1"
+  storage_mb          = 5120
+  administrator_login = var.db_admin_user
+  administrator_login_password = var.db_admin_password
+  version = "11"
 }
