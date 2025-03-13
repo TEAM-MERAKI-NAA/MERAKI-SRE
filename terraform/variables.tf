ae18691b1c2437b18603555910823ea65c877c1e
Sandesh Dhakal <sandesh841maple@gmail.com>
2025-03-12 22:17:19 -0400

    Update in the  variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,38 +1,46 @@
+# giving the prefix for resource name 
 variable "prefix" {
   description = "Prefix for resource names"
   type        = string
 }
 
+# Declaring the location 
 variable "location" {
   description = "Azure location"
   type        = string
 }
 
+#CIDR for Public Virtual Network
 variable "public_vnet_cidr" {
   description = "CIDR block for the Public Virtual Network"
   type        = string
 }
 
+#CIDR for Private Virtual Network
 variable "private_vnet_cidr" {
   description = "CIDR block for the Private Virtual Network"
   type        = string
 }
 
+#CIDR for Public Subnet
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
 }
 
+#CIDR for Private Subnet
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   type        = string
 }
 
+#Database admin user 
 variable "db_admin_user" {
   description = "Database administrator username"
   type        = string
 }
 
+#Database admin password 
 variable "db_admin_password" {
   description = "Database administrator password"
   type        = string
