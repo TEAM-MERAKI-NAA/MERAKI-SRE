a7c0711d09349dc73413aebe5e600d581c5b8b27
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-14 16:36:22 -0400

    cleanup

---
+++ structure/variables.tf
@@ -0,0 +1,23 @@
+variable "prefix" {
+  description = "The prefix used for all resources"
+  type        = string
+  default     = "myapp"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "admin_username" {
+  description = "Username for the VM"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
