34629a4ef089c6005239b5268da77f63d5aac25d
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-20 22:32:46 -0400

    necessary update in outputs.tf

--- terraform_config/outputs.tf
+++ terraform_config/outputs.tf
@@ -1,3 +1,13 @@
+# SSH Key Outputs
+output "ssh_private_key" {
+  value     = tls_private_key.ssh.private_key_pem
+  sensitive = true
+}
+
+output "ssh_public_key" {
+  value = tls_private_key.ssh.public_key_openssh
+}
+
 # ACR Outputs
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
@@ -19,4 +29,26 @@ output "vm_public_ip" {
 
 output "vm_username" {
   value = var.admin_username
+}
+
+# Application URLs
+output "application_urls" {
+  value = {
+    react_app  = "http://${azurerm_public_ip.vm_public_ip.ip_address}:3000"
+    django_app = "http://${azurerm_public_ip.vm_public_ip.ip_address}:8000"
+  }
+}
+
+# Resource Information
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "virtual_machine_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+# Connection String
+output "ssh_connection_string" {
+  value = "ssh ${var.admin_username}@${azurerm_public_ip.vm_public_ip.ip_address}"
 } 
