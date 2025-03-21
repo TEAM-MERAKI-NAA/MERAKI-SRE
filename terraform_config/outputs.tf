f9fbb05cb9303554ea5d447d17924d8871d0f62b
Sdhakal24 <sandesh841maple@gmail.com>
2025-03-20 22:40:50 -0400

    synchronized way of outputs.tf

--- terraform_config/outputs.tf
+++ terraform_config/outputs.tf
@@ -1,11 +1,6 @@
-# SSH Key Outputs
-output "ssh_private_key" {
-  value     = tls_private_key.ssh.private_key_pem
-  sensitive = true
-}
-
-output "ssh_public_key" {
-  value = tls_private_key.ssh.public_key_openssh
+# Resource Group Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
 }
 
 # ACR Outputs
@@ -22,13 +17,44 @@ output "acr_password" {
   sensitive = true
 }
 
-# VM Outputs
-output "vm_public_ip" {
+# Network Outputs
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.public_subnet.name
+}
+
+output "public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
+}
+
+output "nsg_name" {
+  value = azurerm_network_security_group.vm_nsg.name
+}
+
+output "nic_name" {
+  value = azurerm_network_interface.vm_nic.name
+}
+
+# VM Outputs
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
 }
 
 output "vm_username" {
   value = var.admin_username
+}
+
+# SSH Key Outputs
+output "ssh_private_key" {
+  value     = tls_private_key.ssh.private_key_pem
+  sensitive = true
+}
+
+output "ssh_public_key" {
+  value = tls_private_key.ssh.public_key_openssh
 }
 
 # Application URLs
@@ -37,15 +63,6 @@ output "application_urls" {
     react_app  = "http://${azurerm_public_ip.vm_public_ip.ip_address}:3000"
     django_app = "http://${azurerm_public_ip.vm_public_ip.ip_address}:8000"
   }
-}
-
-# Resource Information
-output "resource_group_name" {
-  value = azurerm_resource_group.main.name
-}
-
-output "virtual_machine_name" {
-  value = azurerm_linux_virtual_machine.vm.name
 }
 
 # Connection String
